d25de947921915d04ab5e284043cfb504e7772d1
a-igo <a.igo@mail.ru>
2016-08-30 19:58:20 +0300

    third version

--- src/Exeptions/Main.java
+++ src/Exeptions/Main.java
@@ -1,1 +1,1 @@
-package Exeptions;public class Main {    public static void main(String[] args) {        System.out.println("hello");    }}
+package Exeptions;public class Main {    public static void main(String[] args) {        System.out.println("hello");        System.out.println("hi");    }}
